66efa877c55430a25da53edc8a706b5998021666
Justin-p <justin-p@users.noreply.github.com>
2020-10-06 14:26:10 +0200

    added files

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo echo ''
+echo 'PREPARE [Installing Ansible.] *********************************'
+echo ''
+sudo apt-get update -y >/dev/null 2>&1
+sudo apt-get install git curl python3 python3-pip -y >/dev/null 2>&1
+pip3 install ansible > /dev/null 2>&1
+git clone https://github.com/justin-p/SetupPythonDev /tmp/repo_folder >/dev/null 2>&1
+export PATH=$PATH:/$HOME/.local/bin
+
+# echo 'PREPARE [Installing requirements with Ansible-Galaxy.] ********'
+# echo ''
+# ansible-galaxy install -r /tmp/repo_folder/files/requirements.yml >/dev/null 2>&1
+echo 'RUN [Running playbook.] ***************************************'
+ansible-playbook /tmp/repo_folder/main.yml -i /tmp/repo_folder/inventory.yml
+
+echo -e '\e[35m[WARNING]: Please reboot the system to apply all changes.'
+echo ''
